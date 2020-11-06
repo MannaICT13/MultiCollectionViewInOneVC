bf38282d3a219c3d3c7a31765697ac6e289fbaba
MannaICT13 <khmannaict13@gmail.com>
2020-11-06 20:14:39 +0600

    Created CollectionView and cell and there outlets

---
+++ MultiCollectionViewInOneVC/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  MultiCollectionViewInOneVC
+//
+//  Created by Md Khaled Hasan Manna on 6/11/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var bgImgCell: UIImageView!
+    
+    
+    
+}
