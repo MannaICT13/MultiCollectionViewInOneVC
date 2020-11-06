4f51d0e94f756fe580d929f2116569610ce62439
MannaICT13 <khmannaict13@gmail.com>
2020-11-06 20:01:09 +0600

    Initial Commit

---
+++ MultiCollectionViewInOneVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiCollectionViewInOneVC
+//
+//  Created by Md Khaled Hasan Manna on 6/11/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
